1a8e8c81123c01e0d598c5e29fc1264baca29e94
Rongmario <rongmario@proton.me>
2026-02-16 23:46:39 +0000

    Simplify the feature of suppressing AgriCraft's model errors

--- src/main/java/com/infinityraider/agricraft/utility/ModelErrorSuppressor.java
+++ src/main/java/com/infinityraider/agricraft/utility/ModelErrorSuppressor.java
@@ -5,11 +5,14 @@ package com.infinityraider.agricraft.utility;
 import com.agricraft.agricore.config.AgriConfigCategory;
 import com.agricraft.agricore.config.AgriConfigurable;
 import com.agricraft.agricore.core.AgriCore;
+import com.agricraft.agricore.log.AgriLogger;
 import com.agricraft.agricore.util.TypeHelper;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import infinityraider.infinitylib.Tags;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelBakeEvent;
@@ -25,10 +28,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class ModelErrorSuppressor {
 
-    private static final List<String> IGNORES = TypeHelper.asList(
-            "agricraft",
-            "agricraftitem"
-    );
+    private static final List<String> IGNORES = TypeHelper.asList(Tags.MOD_ID, Tags.MOD_ID + "item");
 
     @AgriConfigurable(key = "Suppress Model Errors", category = AgriConfigCategory.DEBUG, comment = "Set to true to prevent any annoying AgriCraft model loading errors from spamming the log.")
     private static boolean supressErrors = true;
@@ -36,17 +36,21 @@ public class ModelErrorSuppressor {
     @SubscribeEvent
     public void onModelBakePost(ModelBakeEvent event) {
         if (supressErrors) {
-            Map<ResourceLocation, Exception> modelErrors = (Map<ResourceLocation, Exception>) ReflectionHelper.getPrivateValue(ModelLoader.class, event.getModelLoader(), "loadingExceptions");
-            Set<ModelResourceLocation> missingVariants = (Set<ModelResourceLocation>) ReflectionHelper.getPrivateValue(ModelLoader.class, event.getModelLoader(), "missingVariants");
+            Map<ResourceLocation, Exception> loadingExceptions = ReflectionHelper.getPrivateValue(ModelLoader.class, event.getModelLoader(), "loadingExceptions");
+            Set<ModelResourceLocation> missingVariants = ReflectionHelper.getPrivateValue(ModelLoader.class, event.getModelLoader(), "missingVariants");
 
-            List<ResourceLocation> errored = modelErrors.keySet().stream().filter((r) -> IGNORES.contains(r.getNamespace())).collect(Collectors.toList());
+            int exceptionStartingCount = loadingExceptions.size();
+            int missingStartingCount = missingVariants.size();
+
+            List<ResourceLocation> errored = loadingExceptions.keySet().stream().filter((r) -> IGNORES.contains(r.getNamespace())).collect(Collectors.toList());
             List<ResourceLocation> missing = missingVariants.stream().filter((r) -> IGNORES.contains(r.getNamespace())).collect(Collectors.toList());
 
-            errored.forEach(modelErrors::remove);
-            missing.forEach(missingVariants::remove);
+            loadingExceptions.entrySet().removeIf(e -> IGNORES.contains(e.getKey().getNamespace()));
+            missing.removeIf(rl -> IGNORES.contains(rl.getNamespace()));
 
-            AgriCore.getLogger("agricraft").info("Suppressed {0} Model Loading Errors!", errored.size());
-            AgriCore.getLogger("agricraft").info("Suppressed {0} Missing Model Variants!", missing.size());
+            AgriLogger logger = AgriCore.getLogger(Tags.MOD_ID);
+            logger.info("Suppressed {0} Model Loading Errors!", exceptionStartingCount - errored.size());
+            logger.info("Suppressed {0} Missing Model Variants!", missingStartingCount - missing.size());
         }
     }
 
